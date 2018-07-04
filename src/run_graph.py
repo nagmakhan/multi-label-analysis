4db027b0e2c7a360f49298436906b41e49dc6c1c
nsk1001 <knagma73@gmail.com>
2018-07-04 16:12:24 +0530

    Update run_graph.py

--- src/run_graph.py
+++ src/run_graph.py
@@ -1,37 +1,36 @@
 import graphcnn.setup.ucmerced as sc
 from graphcnn.experiment_multilabel import *
 
- os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 
-dataset = sc.load_ucmerced_dataset()
+dataset = sc.load_ucmerced_dataset() # to load the data
 
 class GraphExperiment():
     def create_network(self, net, input):
         net.create_network(input)
         net.make_embedding_layer(256) #takes input of any cardinality and produces output of fixed size
         net.make_dropout_layer()
-        net.make_graphcnn_layer(128) #earlier 64, then 128 for best performance
+        net.make_graphcnn_layer(128) 
         net.make_dropout_layer()
-        net.make_graph_embed_pooling(no_vertices=64) #earlier 32, then 64 for best performance
+        net.make_graph_embed_pooling(no_vertices=64) 
         net.make_dropout_layer()
             
-        net.make_graphcnn_layer(64) #earlier 32, then 64 for best performance
+        net.make_graphcnn_layer(64) 
         net.make_dropout_layer()
-        net.make_graph_embed_pooling(no_vertices=32) #earlier 8, then 16 for best performance, 32 for later best
+        net.make_graph_embed_pooling(no_vertices=32) 
         net.make_dropout_layer()
         
-        net.make_fc_layer(256) #later best, earlier 256
+        net.make_fc_layer(256) 
         net.make_dropout_layer()
         net.make_fc_layer(17, name='final', with_bn=False, with_act_func = False)
         
-exp = GraphCNNExperiment('Graph', 'graph', GraphExperiment())
+exp = GraphCNNExperiment('Graph', 'graph', GraphExperiment()) 
 
-exp.num_iterations = 250000
-exp.optimizer = 'momentum'
-exp.debug = True
+exp.num_iterations = 250000 #num of iterations
+exp.optimizer = 'momentum' #optimizer
+exp.debug = True #verbose mode
         
-exp.preprocess_data(dataset)
-beta = 1.0
+exp.preprocess_data(dataset) #preprocess data (zero pad adjacency matrix,etc)
+beta = 1.0  #for weighing precision and recall in f-score
 
-acc = exp.run_experiments(beta=beta, threshold = 0.5)
+acc = exp.run_experiments(beta=beta, threshold = 0.5) #run experiment, pass threshold for classification
 print_ext('Accuracy(f-score): %.2f' % acc)
